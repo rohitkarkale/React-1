fa060dcc333bc88605e6afdd4afb724aaf618ea7
rohitkarkale <160994340+rohitkarkale@users.noreply.github.com>
2024-03-10 10:59:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 
 import "./App.css";
-import MainApp from "./component/MainApp";
-import Header from "./component/Header";
-import Footer from "./component/Footer";
-import AddUser from "./component/AddUser";
-// import RestApp from "./component/RestApp";
+// import MainApp from "./component/MainApp";
+// import Header from "./component/Header";
+// import Footer from "./component/Footer";
+// import AddUser from "./component/AddUser";
+import RestApp from "./component/RestApp";
 
 function App() {
   return (
     <div className="App">
 
       <p>This is header section</p>
-      <Header/>
+      {/* <Header/>
       <AddUser></AddUser>
-      <MainApp />
-      {/* <RestApp/> */}
-      <Footer/>
+      <MainApp /> */}
+      <RestApp/>
+
+      {/* <Footer/> */}
     </div>
   );
 }
